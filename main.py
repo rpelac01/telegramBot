71177048574b0f6f8678f7ac7a7835272ea1c1ce
Ruben <rubenpelayo86@gmail.com>
2026-06-22 23:05:20 +0200

    Primera fase acabada

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def enviar_bienvenida(mensaje):
     bot.reply_to(mensaje, "Hola, vamos a controlar tus gastos")
 print("Bot encendido")
 bot.infinity_polling()
+
+@bot.message_handler(commands = ['gasto'])
+def registro_gasto(mensaje):
+    trozos = mensaje.text.split("", 2)
+    if len(trozos)<3:
+        bot.reply_to(mensaje, "⚠️ Error. Úsalo así: /gasto [cantidad] [concepto]")
+        return
+    cantidad = trozos[1]
+    concepto = trozos[2]
+    bot.reply_to(mensaje, f"✅ ¡Apuntado! Has gastado {cantidad}€ en: {concepto}")
